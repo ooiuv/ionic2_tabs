10554a5851c230928e40fc8302bd00314b77a8a0
yanxiaojun617 <yanxiaojun617@163.com>
2016-12-31 00:09:15 +0800

    上传ionic2_tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,22 +1,20 @@
 import {Component, ViewChild} from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import {HomePage} from '../home/home';
+import {AboutPage} from '../about/about';
+import {ContactPage} from '../contact/contact';
 import {Tabs} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  @ViewChild('mainTabs') tabs:Tabs;
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
+  @ViewChild('mainTabs') tabs: Tabs;
   tab1Root: any = HomePage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 
   constructor() {
-
   }
+
 }
