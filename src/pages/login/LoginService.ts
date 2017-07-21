d3264dada0b1395509223e5ed5c77b992239a4a4
yanxiaojun617 <yanxiaojun617@163.com>
2017-07-21 17:51:48 +0800

    升级app

--- src/pages/login/LoginService.ts
+++ src/pages/login/LoginService.ts
@@ -12,24 +12,27 @@ export class LoginService {
 
 
   login(user): Observable<(LoginInfo)> {
-    let param = {
-      'client_id': 'app',
-      'username': user.username,
-      'password': hex_md5(user.password)
+    /* let param = {
+     'client_id': 'app',
+     'username': user.username,
+     'password': hex_md5(user.password)
+     };
+     return this.httpService.post('/authenticate', param).map((res: Response) => res.json());*/
+    let loginInfo = {
+      access_token: 'JhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMiIsImNsaWVudF9pZ…a1xwEVxvsZj8Xuar__68EbM7MZ8tSSIAvfUZ7HbCpI-wT',
+      user: {
+        id: 1,
+        username: user.username,
+        name: '小军',
+        email: 'yanxiaojun617@163.com',
+        phone: '18688498342',
+        avatarId: '',
+        description: '有图有真相，一本正经的胡说八道..'
+      }
     };
-    return this.httpService.post('/authenticate', param).map((res: Response) => res.json());
-    // let userInfo = {
-    //   id: 1,
-    //   username: user.username,
-    //   name: '小军',
-    //   email: 'yanxiaojun617@163.com',
-    //   phone: '18688498342',
-    //   avatarId: '',
-    //   description: '有图有真相，一本正经的胡说八道..'
-    // };
-    // return Observable.create((observer) => {
-    //   observer.next(userInfo);
-    // });
+    return Observable.create((observer) => {
+      observer.next(loginInfo);
+    });
   }
 
 }
