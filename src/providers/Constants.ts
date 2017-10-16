eb06c16c406a4825018448b44ca2ee939d8e62ad
yanxiaojun617 <yanxiaojun617@163.com>
2017-10-16 09:44:37 +0800

    更改配置

--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -7,7 +7,7 @@ export const FILE_SERVE_URL = 'http://172.16.19.86/kit_file_server/';//文件服
 /*----------------------------------------app版本升级服务地址----------------------------------------*/
 export const APP_VERSION_SERVE_URL = 'http://172.16.19.86:8111/api/';//app版本升级服务;测试环境,查询app最新版本号,更新日志等信息.
 
-export const IS_DEBUG = false;//是否开发(调试)模式
+export const IS_DEBUG = true;//是否开发(调试)模式
 
 export const DEFAULT_AVATAR = './assets/img/avatar.png';//用户默认头像
 export const PAGE_SIZE = 5;//默认分页大小
@@ -16,7 +16,7 @@ export const QUALITY_SIZE = 94;//图像压缩质量，范围为0 - 100
 export const REQUEST_TIMEOUT = 12000;//请求超时时间,单位为毫秒
 
 
-export const ENABLE_FUNDEBUG = true;//是否启用fundebug日志监控
+export const ENABLE_FUNDEBUG = false;//是否启用fundebug日志监控
 export const FUNDEBUG_API_KEY = '3701a358f79b7daa39592255bde6c3c8772efad642883e42dbb65f3f8ffbae11';//去https://fundebug.com/申请key
 
 export const APK_DOWNLOAD = 'http://omzo595hi.bkt.clouddn.com/ionic2_tabs.apk';//android apk下载完整地址,用于android本地升级
