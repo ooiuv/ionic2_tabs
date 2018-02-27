43f57752e90e3ed8e840dcbbd5919bcb4d8eecfc
yanxiaojun617 <yanxiaojun617@163.com>
2018-02-27 17:04:09 +0800

    feat:删除冗余代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule, ErrorHandler, LOCALE_ID} from "@angular/core";
+import {NgModule, ErrorHandler} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {IonicApp, IonicModule, Config} from "ionic-angular";
 import {IonicStorageModule} from "@ionic/storage";
