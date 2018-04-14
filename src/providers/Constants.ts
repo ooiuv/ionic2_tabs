3e8a142b9f6db5f85d95aaf6c048a0872004a5f8
yanxiaojun617 <yanxiaojun617@163.com>
2018-04-14 15:52:45 +0800

    refactor:重构HttpService

--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -10,7 +10,7 @@ export const FILE_SERVE_URL = 'http://172.16.19.86/kit_file_server/';//文件服
 // export const APP_VERSION_SERVE_URL = 'http://172.16.19.86:8111/api/';//原测试环境
 export const APP_VERSION_SERVE_URL = 'http://172.16.19.86/version/api/';//新测试环境
 
-export const IS_DEBUG = false;//是否开发(调试)模式
+export const IS_DEBUG = true;//是否开发(调试)模式
 
 export const DEFAULT_AVATAR = './assets/img/avatar.png';//用户默认头像
 export const PAGE_SIZE = 5;//默认分页大小
