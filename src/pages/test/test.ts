ec6713cde0acdd8d6239b9a31f212f13681be3a0
yanxiaojun <6177160Kt>
2018-11-13 14:31:10 +0800

    fix:可编辑div在ios上bug

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { TestService } from './TestService';
 import { FileObj } from '../../model/FileObj';
 import { NativeService } from '../../providers/NativeService';
-import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-test',
@@ -11,11 +10,8 @@ import { Platform } from 'ionic-angular';
 export class TestPage {
 
   fileObjList: FileObj[] = [];
-  test = '12';
-  isAndroid;
 
-  constructor(public testService: TestService, public nativeService: NativeService, private platform: Platform) {
-    this.isAndroid = this.platform.is('android');
+  constructor(public testService: TestService, public nativeService: NativeService) {
   }
 
   getFileData() {
